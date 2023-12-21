62310f2758c2a071243efc0aeb32bef5ad3f94df
Glassalo <maxence.labourel@epitech.eu>
2023-12-21 14:02:34 +0100

    Refactor CMakeLists and add Exception class

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,14 +5,28 @@ include(cmake/CPM.cmake)
 # targets
 
 find_package(Catch2 QUIET)
+find_package(spdlog QUIET)
+find_package(Boost QUIET)
+
+message(STATUS "Catch2_FOUND: ${Catch2_FOUND}")
+message(STATUS "spdlog_FOUND: ${spdlog_FOUND}")
+message(STATUS "Boost_FOUND: ${Boost_FOUND}")
 
 function(patatocs_setup_dependencies)
 
-  # For each dependency, see if it's
-  # already been provided to us by a parent project
+    # For each dependency, see if it's
+    # already been provided to us by a parent project
 
-  if(NOT TARGET Catch2::Catch2WithMain AND Catch2_FOUND EQUAL 0)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
-  endif()
+    if(NOT TARGET Catch2::Catch2WithMain AND Catch2_FOUND EQUAL 0)
+        cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    endif()
+
+    if(NOT TARGET spdlog::spdlog AND spdlog_FOUND EQUAL 0)
+        cpmaddpackage("gh:gabime/spdlog@1.12.0")
+    endif()
+
+    if(NOT TARGET Boost::boost AND Boost_FOUND EQUAL 0)
+        cpmaddpackage("gh:boostorg/boost@1.78.0")
+    endif()
 
 endfunction()
