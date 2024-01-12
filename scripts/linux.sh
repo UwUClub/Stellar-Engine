e0e834358187de46de6466578184eebb24aa4faa
Glassalo <maxence.labourel@epitech.eu>
2024-01-12 15:31:45 +0100

    Cahnged deps

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,20 +2,10 @@
 
 # get package manager
 if [ -x "$(command -v apt-get)" ]; then
-    package_manager="apt-get install"
-    no_confirm="-y"
-elif [ -x "$(command -v yum)" ]; then
-    package_manager="yum insta;;"
-    no_confirm="-y"
+    apt-get install -y libboost-all-dev libspdlog-dev libcatch2-dev
 elif [ -x "$(command -v pacman)" ]; then
-    package_manager="pacman -S"
-    no_confirm="--noconfirm"
+    pacman -S --noconfirm git boost spdlog catch2
 else
     echo "No package manager found. Exiting."
     exit 1
 fi
-
-# install packages
-echo "Installing packages..."
-echo $package_manager
-sudo $package_manager $no_confirm git boost spdlog catch2
