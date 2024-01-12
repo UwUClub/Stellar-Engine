9e8ee7243108fd5dba39b17ac255e45df1057a4d
Glassalo <maxence.labourel@epitech.eu>
2024-01-12 15:41:18 +0100

    Changed deps install

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,9 +2,9 @@
 
 # get package manager
 if [ -x "$(command -v apt-get)" ]; then
-    sudo apt-get install -y libboost-all-dev libspdlog-dev catch2
+    sudo apt-get install -y libboost-all-dev
 elif [ -x "$(command -v pacman)" ]; then
-    sudo pacman -S --noconfirm git boost spdlog catch2
+    sudo pacman -S --noconfirm boost
 else
     echo "No package manager found. Exiting."
     exit 1
