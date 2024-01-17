92c38aa5444686a89ce44b4e2ddf8bae63ddab1d
Glassalo <maxence.labourel@epitech.eu>
2024-01-17 16:17:28 +0100

    Added last tests for query for currently made methods

--- test/tests.cpp
+++ test/tests.cpp
@@ -231,4 +231,47 @@ TEST_CASE("Query")
         REQUIRE(query[0] == 0);
         REQUIRE(query[1] == 2);
     }
+
+    SECTION("getComponentsOfEntity")
+    {
+        world.registerComponents<hp1, hp2, Player>();
+
+        world.createEntity();
+
+        world.addComponentToEntity(0, hp1 {hps});
+        world.addComponentToEntity(0, hp2 {hps});
+        world.addComponentToEntity(0, Player {});
+
+        auto query = world.query<hp1, hp2, Player>().getComponentsOfEntity(0);
+        const hp1 &hp1Comp = std::get<0>(query);
+        const hp2 &hp2Comp = std::get<1>(query);
+        const Player &playerComp = std::get<2>(query);
+
+        REQUIRE(hp1Comp.hp == hps);
+        REQUIRE(hp2Comp.maxHp == hps);
+        REQUIRE_NOTHROW(playerComp);
+    }
+
+    SECTION("getAll")
+    {
+        world.registerComponents<hp1, hp2, Player>();
+
+        auto idx1 = world.createEntity();
+        world.createEntity();
+        auto idx2 = world.createEntity();
+
+        world.addComponentToEntity(0, hp1 {hps});
+        world.addComponentToEntity(1, hp1 {hps});
+        world.addComponentToEntity(2, hp1 {hps});
+        world.addComponentToEntity(0, hp2 {hps});
+        world.addComponentToEntity(1, hp2 {hps});
+        world.addComponentToEntity(2, hp2 {hps});
+        world.addComponentToEntity(0, Player {});
+        world.addComponentToEntity(2, Player {});
+
+        auto query = world.query<hp1, hp2, Player>().getAll();
+        REQUIRE(query.size() == 2);
+        REQUIRE(std::get<0>(query[0]) == idx1);
+        REQUIRE(std::get<0>(query[1]) == idx2);
+    }
 }
